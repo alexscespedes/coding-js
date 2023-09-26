c20404d566777320ecc7413d94292eb304bcd9be
alexscespedes <alexandersenc99@gmail.com>
2023-09-25 20:36:09 -0400

    JavaScript addEventListener

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const h1 = document.querySelector('h1')
+const form = document.querySelector('#form')
 const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
 const pResult = document.querySelector('#result');
 
-function onChange() {
-    console.log('cambio el input')
+form.addEventListener('submit', submitValues)
+
+function submitValues(event) {
+    // console.log({event})
+    event.preventDefault();
+    const sumaInputs = parseInt(input1.value) + parseInt(input2.value);
+    pResult.innerHTML = "Resultado: " + sumaInputs;
+
+    
 }
 
-function btnOnclick() {
+
+btn.addEventListener('click', btnOnClick)
+
+function btnOnClick(event) {
+    // console.log({event})
+    event.preventDefault();
     const sumaInputs = parseInt(input1.value) + parseInt(input2.value);
     pResult.innerHTML = "Resultado: " + sumaInputs;
 
